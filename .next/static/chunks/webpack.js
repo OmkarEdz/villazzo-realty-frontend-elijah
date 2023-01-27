5376909a394f84507a492077b2ac31aade6b3a51
OmkarEdz <omkar.t@edreamz.in>
2023-01-27 09:53:51 +0530

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08df0bf0579884a2"; }
+/******/ 		__webpack_require__.h = function() { return "d94cab20d6718b6b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/search-new.js
+++ pages/search-new.js
@@ -3,18 +3,38 @@ import Footer from "../components/footer"
 import Header from "../components/header"
 import { fetchAPI } from "../lib/api"
 import Script from 'next/script'
+import { Range } from "react-range"
+import Link from "next/link"
+import { useRouter } from 'next/router'
 
 const SearchNew = ({ global, homepage, footerData, navigation }) => {
   const myLoader = ({ src, width, quality }) => {
     return `${src}?w=${width}&q=${quality || 75}`
   }
+  const router = useRouter()
+  let defaultState = { values: [100] }
+  let [state, setState] = useState(defaultState)
+  const [location, setLocation] = useState("")
+  const [propertyType, setPropertyType] = useState("8")
+  const currencyFormat = (num)=> {
+    return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
   const MBB = { mbbUrl: 'https://www.mbb2.com/version3', seo : 'false' };
   MBB.data = { acid : 'qwXQVWE' };
+
+  const MBBv3_SubmitCustomSearchForm = (event) => {
+    event.preventDefault();
+    let searchURL = 'http://www.villazzorealty.com/results'
+    router.push(searchURL)
+  }
   return (
     <>
-    <Script src='https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBjUILCWnup4zgs3JZJF6gysN4KAK5FwTQ' />
     <Script src='https://www.mbb2.com/version3/css/theme/acid/qwXQVWE' />
+    
+    <Script src='https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBjUILCWnup4zgs3JZJF6gysN4KAK5FwTQ' />
     <Script src='https://d2w6u17ngtanmy.cloudfront.net/scripts/my-buying-buddy.5.0.js.gz' />
+    
       <div className="bg-img">
         <div className="homepage-image">
           <div className="blankDiv"></div>
@@ -24,14 +44,94 @@ const SearchNew = ({ global, homepage, footerData, navigation }) => {
           </video>
         </div> 
         <Header navigation={navigation} global={global} />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
         <div className="buttons-wrap">
           <div className="sell-search-container buy">
             <div className="selltxt">
               <h3 className="propertytxt">THE PERFECT HOME FOR YOU</h3>
               <p>Villazzo is here to help acquire your dream home or condo. We have helped many clients successfully secure their new residence in South Florida. Let us do the same for you.</p>
             </div>
-            <div className="marginTopSearchNew"><div id="MBBv3_SearchForm"></div></div>
+            <form id="Form-1">
+                <input type="hidden" name="mls_id" value="fl133" />
+                <div className="row">
+                  <div className="col-md-4">
+                    <label className="">LOCATION</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      onChange={(e) => setLocation(e.target.value)}
+                      placeholder="CITY, REGION"
+                    />
+                  </div>
+                  <div className="col-md-3 prop_type">
+                    <label className="">PROPERTY TYPE</label>
+                    <select
+                      className="form-control"
+                      placeholder="PROPERTY TYPE"
+                      onChange={(e) => setPropertyType(e.target.value)}
+                    >
+                      <option value="8">Commercial</option>
+                      <option value="2">Condo/Townhouse/Co-Op</option>
+                      <option value="4">Farms/Ranch</option>
+                      <option value="9">Lot/Land/Acreage</option>
+                      <option value="3">Mobile/Manufactured</option>
+                      <option value="6">Multi Family</option>
+                      <option value="5">Rental Properties</option>
+                      <option value="7">Residential Income</option>
+                      <option value="7">Single Family</option>
+                      <option value="10">Vacation/Time-Share</option>
+                    </select>
+                  </div>
+                  <div className="col-md-4">
+                    <label className="">PRICE RANGE</label>
+                    <Range
+                      step={100}
+                      initial={100}
+                      min={100}
+                      max={250000000}
+                      values={state.values}
+                      onChange={(values) => setState({ values })}
+                      renderTrack={({ props, children }) => (
+                        <div
+                          {...props}
+                          style={{
+                            ...props.style,
+                            height: "4px",
+                            width: "100%",
+                            marginTop: "15px",
+                            backgroundColor: "#f7dd80",
+                          }}
+                        >
+                          {children}
+                        </div>
+                      )}
+                      renderThumb={({ props }) => (
+                        <div
+                          {...props}
+                          style={{
+                            ...props.style,
+                            height: "15px",
+                            width: "15px",
+                            borderRadius: "25px",
+                            backgroundColor: "#fff",
+                            borderColor: "#f7dd80",
+                            borderStyle: "solid",
+                            borderWidth: "4px",
+                          }}
+                        />
+                      )}
+                    />
+                    <output style={{ marginTop: "5px", float:"right" }} id="output">
+                      {currencyFormat(state.values[0])}
+                    </output>
+                  </div>
+                  <div className="col-md-1 search-icon-box">
+                  <input name="submit" type="button" onclick="MBBv3_SubmitCustomSearchForm('Form-1','http://www.charliesmithrealty.com/results');" value="Search!"  />
+                  </div>
+                </div>  
+              </form>
+              
+
+
           </div>
         </div>
       </div>
