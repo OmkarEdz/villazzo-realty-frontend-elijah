79caeb685a16270153478cdf9d01972e55ffe3b1
OmkarEdz <omkar.t@edreamz.in>
2023-06-15 11:08:50 +0530

    Updated files

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/property-detail":["static\u002Fchunks\u002Fpages\u002Fproperty-detail.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fproperty-detail"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
