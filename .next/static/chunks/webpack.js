b76b35536f2507b3b9f55e19783947c8bf8ae51d
OmkarEdz <omkar.t@edreamz.in>
2023-07-13 20:14:41 +0530

    Updated css for feedback

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2215e0d7082706b9"; }
+/******/ 		__webpack_require__.h = function() { return "e32e557f81a479c5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/property-card.js
+++ components/property-card.js
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from "react"
-import NextImage from "./image"
-import axios from 'axios';
+// import React, { useState, useEffect } from "react"
+// import NextImage from "./image"
+// import axios from 'axios';
 
-const PropertyCard = () => {
-  const [data, setData] = useState([]);
+// const PropertyCard = () => {
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('http://localhost:1337/api/properties'); // Replace with the actual endpoint for your collection
-      setData(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  console.log(data);
-  return (
-    <>
-      <div className="property-wrap property-list-wrap">
-        {properties.map((property) => (
-        <div className="property-box">
-            <p className="img_Wrap">
-              <NextImage image={property.attributes.image} />
-            </p>
-            <h4 className="pro-name">{property.attributes.location}</h4>
-            <div className="pro-text">{property.attributes.Content}</div>
-        </div>
-        ))}
-      </div>
-    </>
-  )
-}
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get('http://localhost:1337/api/properties'); // Replace with the actual endpoint for your collection
+//       setData(response.data);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   console.log(data);
+//   return (
+//     <>
+//       <div className="property-wrap property-list-wrap">
+//         {properties.map((property) => (
+//         <div className="property-box">
+//             <p className="img_Wrap">
+//               <NextImage image={property.attributes.image} />
+//             </p>
+//             <h4 className="pro-name">{property.attributes.location}</h4>
+//             <div className="pro-text">{property.attributes.Content}</div>
+//         </div>
+//         ))}
+//       </div>
+//     </>
+//   )
+// }
 
-export default PropertyCard
+// export default PropertyCard
