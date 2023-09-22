7c8edcb33cfedb2df792af19cfd01b5c66106e22
OmkarEdz <omkar.t@edreamz.in>
2023-09-22 12:02:45 +0530

    Removed SEO code

--- pages/_app.js
+++ pages/_app.js
@@ -18,9 +18,8 @@ const MyApp = ({ Component, pageProps }) => {
   // }, 2500);  
   return (
     <>
-      <Head>
-        <script type="text/javascript" src="data:text/javascript;base64,LyogQWxsaSBBSSB3aWRnZXQgZm9yIHZpbGxhenpvLmNvbSAqLwooZnVuY3Rpb24gKHcsZCxzLG8sZixqcyxmanMpIHt3WydBbGxpSlNXaWRnZXQnXT1vO3dbb10gPSB3W29dIHx8IGZ1bmN0aW9uICgpIHsgKHdbb10ucSA9IHdbb10ucSB8fCBbXSkucHVzaChhcmd1bWVudHMpIH07anMgPSBkLmNyZWF0ZUVsZW1lbnQocyksIGZqcyA9IGQuZ2V0RWxlbWVudHNCeVRhZ05hbWUocylbMF07anMuaWQgPSBvOyBqcy5zcmMgPSBmOyBqcy5hc3luYyA9IDE7IGZqcy5wYXJlbnROb2RlLmluc2VydEJlZm9yZShqcywgZmpzKTt9KHdpbmRvdywgZG9jdW1lbnQsICdzY3JpcHQnLCAnYWxsaScsICdodHRwczovL3N0YXRpYy5hbGxpYWkuY29tL3dpZGdldC92MS5qcycpKTthbGxpKCdpbml0JywgJ3NpdGVfQmlQUmh1N0ppRHBEUUZGeicpO2FsbGkoJ29wdGltaXplJywgJ2FsbCcpOw=="></script>
-      </Head>
+      {/* <Head>
+      </Head> */}
       <GlobalContext.Provider value={global.attributes}>
         <Component {...pageProps} />
       </GlobalContext.Provider>
