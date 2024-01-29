951296e5ded5471a191c5dcb83d1dc2fe46853e7
OmkarEdz <omkar.t@edreamz.in>
2024-01-29 09:20:50 +0530

    Final Feedback

---
+++ pages/contact.js
@@ -0,0 +1,288 @@
+import React, { useState, useEffect } from "react"
+import Footer from "../components/footer"
+import Header from "../components/header"
+import { fetchAPI } from "../lib/api"
+import axios from 'axios'
+
+
+const Contact = ({
+  global,
+  footerData,
+  navigation,
+}) => {
+  
+  
+  // States for contact form fields
+  const [fullname, setFullname] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onchange = event => {
+    const result = event.target.value.replace(/\D/g, '');
+    setPhone(result);
+  };
+
+  //   Form validation state
+  const [errors, setErrors] = useState({});
+
+  // Setting success or failure messages states
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+  const re = /^[0-9\b]+$/;
+  const handleChange = (e) => {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+      setPhone(e.target.value)
+    }
+  }
+
+  // Validation check method
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (fullname.length <= 0) {
+      tempErrors["fullname"] = true;
+      isValid = false;
+    }
+    if (lastName.length <= 0) {
+      tempErrors["lastName"] = true;
+      isValid = false;
+    }
+    if (email.length <= 0) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+    if (phone.length <= 0) {
+      tempErrors["phone"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log("errors", errors);
+    return isValid;
+  };
+
+  // Handles the submit event on form submit.
+  const handleSubmit = async (event) => {
+  // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+    
+    let isValidForm = handleValidation();
+
+    if (isValidForm) {
+      axios.post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/contactforms`,
+        {
+          "data": {
+            firstName: fullname,
+            lastName: lastName,
+            email: email,
+            phoneNo: phone,
+            message: message,
+          }
+        }
+      );
+
+      const qs = require('qs');
+        axios.post('https://webdevfolio.com/Villazzomail/Villazzomail.php',  qs.stringify({
+            "firstName": fullname,
+            "lastName": lastName,
+            "email": email,
+            "phoneNo": phone,
+            "message": message,
+
+        }))
+        .then((res) => {
+          console.log(`statusCode: ${res.statusCode}`)
+          console.log(res)
+          console.log(`statusCode: ${res.data}`)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+      
+      setShowSuccessMessage(true);
+      setShowFailureMessage(false);
+      // Reset form fields
+      setFullname("");
+      setlastName("");
+      setEmail("");
+      setPhone("");
+      setMessage("");
+
+      return;
+    }
+      setShowSuccessMessage(false);
+      setShowFailureMessage(true);
+      // Reset form fields
+      setFullname("");
+      setlastName("");
+      setEmail("");
+      setPhone("");
+      setMessage("");
+  }
+  return (
+    <>
+      <div className="bg-img bg-white sold_pg_wrap videos_pg_wrap contact_main_pg_wrapper">
+        <Header navigation={navigation} global={global} />
+        <div className="container">
+          <div className="contact_main_pg_wrapper_inner">
+            <h1 className="video-heading">Contact</h1>
+            <div className="contact_icons_section">
+              <div className="contact_icon_item">
+                <p className="contact_icon"><i className="fa-solid fa-phone"></i></p>
+                <p className="contact_text">
+                  <a href="tel:+1 (877) 845-5299" className="mail-hover phoneClicked" title="1-877-VILLAZZO +1-877-845-5299"> 1-877-VILLAZZO<br /> +1-877-845-5299</a>
+                </p>
+              </div>
+              <div className="contact_icon_item">
+                <p className="contact_icon"><i className="fa-solid fa-envelope"></i></p>
+                <p className="contact_text">
+                <a href="mailto:villazzorealty@villazzo.com">VILLAZZOREALTY@VILLAZZO.COM</a>
+                </p>
+              </div>
+              <div className="contact_icon_item">
+                <p className="contact_icon"><i className="fa-solid fa-clock"></i></p>
+                <p className="contact_text">
+                MON -FRI<br/>9AM - 5PM EST
+                </p>
+              </div>
+              <div className="contact_icon_item">
+                <p className="contact_icon"><i className="fa-solid fa-location-dot"></i></p>
+                <p className="contact_text">
+                  <a href="https://goo.gl/maps/w7pPMcrghZ61T7XKA" target="_blank">929 ALTON RD, SUITE 500 <br />MIAMI BEACH, FL 33139</a>
+                </p>
+              </div>
+            </div>
+            <div className="form_pg_wrapper">
+              <form onSubmit={handleSubmit}>
+                        <div className="contact-form">
+                          <div className="contact-form-label">
+                            <div className="form-item">
+                              <input
+                                placeholder="FIRST NAME" 
+                                type="text"
+                                value={fullname}
+                                onChange={(e) => {
+                                  setFullname(e.target.value);
+                                }}
+                                name="fullname"
+                                className="input-name contact-lebel"
+                              />
+                              {errors?.fullname && (
+                                <p className="error_msg">First name cannot be empty.</p>
+                              )}
+                            </div>
+                            <div className="form-item">
+                              <input
+                                placeholder="LAST NAME" 
+                                name="lastName"
+                                type="text"
+                                value={lastName}
+                                onChange={(e) => {
+                                  setlastName(e.target.value);
+                                }}
+                                className="contact-lebel"
+                              />
+                              {errors?.lastName && (
+                                <p className="error_msg">Last name cannot be empty.</p>
+                              )}
+                            </div>
+                          </div>
+                          <div className="contact-form-label">
+                            <div className="form-item">
+                              <input
+                                placeholder="EMAIL ADDRESS" 
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => {
+                                  setEmail(e.target.value);
+                                }}
+                                className="input-name contact-lebel"
+                              />
+                              {errors?.email && (
+                                <p className="error_msg">Email cannot be empty.</p>
+                              )}
+                            </div>
+                            <div className="form-item">
+                              <input
+                                placeholder="PHONE" 
+                                type="tel"
+                                name="phone"
+                                pattern="[0-9]*"
+                                value={phone}
+                                onChange={handleChange}
+                                className="input-name contact-lebel"
+                                maxLength="12"
+                              />
+                              {errors?.email && (
+                                <p className="error_msg">Phone number cannot be empty.</p>
+                              )}
+                            </div>
+                          </div>
+                          <div className="contact-form-label">
+                            <div className="form-item full-width">
+                              <textarea
+                                name="message"
+                                value={message}
+                                onChange={(e) => {
+                                  setMessage(e.target.value);
+                                }}
+                                className="form-message contact-lebel" rows="4" cols="50" placeholder="MESSAGE">
+                              </textarea>
+                            </div>
+                          </div>
+                          <div className="submit_btn_wrap">
+                            <button type="submit" >Submit</button>
+                          </div>
+                          <div className="final_msg_wrap">
+                            {showSuccessMessage && (
+                              <p className="thankyou_msg">
+                                Thank you for your message! A team member from Villazzo Realty will get back to you shortly.
+                              </p>
+                            )}
+                            {showFailureMessage && (
+                              <p className="error_msg">
+                                Please fill the form
+                              </p>
+                            )}
+                          </div>
+                        </div>
+                      </form>
+            </div>
+          </div>	
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3592.717439829409!2d-80.14049399999999!3d25.779895000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b48a561798bf%3A0x540a2fa56605f4f9!2s929%20Alton%20Rd%20%23500%2C%20Miami%20Beach%2C%20FL%2033139%2C%20USA!5e0!3m2!1sen!2sin!4v1706499280598!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <Footer footerProp={footerData} />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  // Run API calls in parallel
+  const [
+    globalRes,
+    footerRes,
+    navigationRes,
+  ] = await Promise.all([
+    fetchAPI("/global", { populate: "*" }),
+    fetchAPI("/footer", { populate: "deep" }),
+    fetchAPI("/header-nav", { populate: "*" }),
+  ])
+
+  return {
+    props: {
+      global: globalRes.data,
+      footerData: footerRes.data,
+      navigation: navigationRes.data,
+    },
+    revalidate: 1,
+  }
+}
+
+export default Contact
